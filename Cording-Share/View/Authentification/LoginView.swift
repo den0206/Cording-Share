f5ff42b678c5c342a0c2ecb892e637c8f9e90139
den0206 <mac.pop@mineo.jp>
2020-12-30 16:37:33 +0900

    Add auth Section

---
+++ Cording-Share/View/Authentification/LoginView.swift
@@ -0,0 +1,91 @@
+//
+//  LoginView.swift
+//  Cording-Share
+//
+//  Created by 酒井ゆうき on 2020/12/30.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    enum loginviewSheet : Identifiable {
+        case signUp, resetPassword
+        
+        var id : Int {
+            hashValue
+        }
+    }
+    
+    @State private var user = AuthUserViewModel()
+    @State private var sheetType : loginviewSheet?
+    
+    var body: some View {
+        
+        VStack(spacing :20) {
+            
+            Spacer()
+        
+            CustomTextField(text: $user.email, placeholder: "Email", imageName: "envelope")
+                
+            CustomTextField(text: $user.password, placeholder: "password", imageName: "lock", isSecure: true)
+            
+           
+            
+            HStack {
+                Spacer()
+                
+                Button(action: {self.sheetType = .resetPassword}) {
+                    Text("Reset Password")
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding(.vertical)
+            .padding(.leading,10)
+            
+            VStack(spacing :10) {
+                
+                Button(action: {}) {
+                    Text("Login")
+                        .foregroundColor(.white)
+                        .padding(.vertical,15)
+                        .frame(width: 200)
+                        .background(Color.green)
+                        .cornerRadius(8)
+                        .opacity(user.isLoginComplete ? 1 : 0.3)
+                }
+                .disabled(!user.isLoginComplete)
+                
+                Button(action: {self.sheetType = .signUp}) {
+                    Text("SignUp")
+                        .foregroundColor(.white)
+                        .padding(.vertical,15)
+                        .frame(width: 200)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                }
+                
+            }
+            .sheet(item: $sheetType) { (item) in
+                
+                switch item {
+                case .signUp :
+                    SignUpView()
+                    
+                case .resetPassword :
+                    Text("Reset Pasword")
+                }
+            }
+            
+            Spacer()
+        }
+        .background(Color(.systemGroupedBackground).ignoresSafeArea(.all, edges: .all) )
+    }
+}
+
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ Cording-Share/View/Authentification/SignUpView.swift
@@ -0,0 +1,166 @@
+//
+//  SignUpView.swift
+//  Cording-Share
+//
+//  Created by 酒井ゆうき on 2020/12/30.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var user = AuthUserViewModel()
+    
+    @State private var showPicker = false
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            ScrollView(.vertical, showsIndicators: false, content: {
+                
+                if !user.selectedImage() {
+                    Text(user.validImageText).font(.caption).foregroundColor(.red)
+                        .padding(.top,5)
+                }
+                
+                Button(action: {showPicker = true}) {
+                    
+                    if user.imageData.count == 0 {
+                        
+                        Image(systemName: "person.crop.circle")
+                            .resizable()
+                            .foregroundColor(.gray)
+                    } else {
+                        
+                        Image(uiImage: UIImage(data: user.imageData)!)
+                            .resizable().scaledToFill().clipShape(Circle())
+                    }
+                  
+                }
+                .frame(width: 90, height: 90)
+                .padding(.vertical, 15)
+                .sheet(isPresented: $showPicker) {
+                    ImagePicker(image: $user.imageData)
+                }
+                
+                Spacer()
+                
+                VStack {
+                    
+                    CustomTextField(text: $user.fullname, placeholder: "Fullname", imageName: "person")
+                    
+                    ValitationText(text: user.validNameText, confirm: !user.validNameText.isEmpty)
+                    
+                    
+                    CustomTextField(text: $user.email, placeholder: "Email", imageName: "envelope")
+                    
+                    
+                    ValitationText(text: user.validEmailText, confirm: !user.validEmailText.isEmpty)
+                    
+                    
+                    CustomTextField(text: $user.password, placeholder: "Password", imageName: "lock")
+                    
+                    
+                    ValitationText(text: user.validPasswordText,confirm: !user.validPasswordText.isEmpty)
+                    
+                    
+                    CustomTextField(text: $user.confirmPassword, placeholder: "Password confirmation", imageName: "lock")
+                    
+                    
+                    ValitationText(text: user.validConfirmPasswordText,confirm: !user.passwordMatch(_confirmPass: user.confirmPassword) )
+                    
+                    
+                    Button(action: {}) {
+                        Text("Register")
+                            .foregroundColor(.white)
+                            .padding(.vertical,15)
+                            .frame(width: 200)
+                            .background(Color.green)
+                            .cornerRadius(8)
+                            .opacity(user.isSignupComplete ? 1 : 0.3)
+                    }
+                    .disabled(!user.isSignupComplete)
+                    .padding()
+                    
+                
+                }
+                
+                Spacer()
+                
+            })
+            /// navigation Property
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(trailing: Button(action: {presentationMode.wrappedValue.dismiss()}, label: {
+                Image(systemName: "xmark")
+                    .foregroundColor(.black)
+            }))
+        }
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
+
+//MARK: - ImagePicker
+
+struct ImagePicker : UIViewControllerRepresentable {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var image : Data
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> some UIImagePickerController {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+    
+    class Coordinator : NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        
+        let parent : ImagePicker
+        
+        init(_ parent : ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            if let image = info[.originalImage] as? UIImage {
+                
+                let data = image.jpegData(compressionQuality: 0.2)
+                
+                let dataKB = Double(data!.count) / 1000.0
+                
+                print(dataKB)
+                if dataKB < 1000.0 {
+                    self.parent.image = data!
+                } else {
+                    return
+                }
+       
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+
+        }
+        
+        
+        
+    }
+    
+}
