3e3cf294bb5974503b1d692e5c743f10338a1a10
den0206 <mac.pop@mineo.jp>
2020-12-30 13:15:18 +0900

    Initial Commit

---
+++ Cording-Share/Cording_ShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cording_ShareApp.swift
+//  Cording-Share
+//
+//  Created by 酒井ゆうき on 2020/12/30.
+//
+
+import SwiftUI
+
+@main
+struct Cording_ShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
