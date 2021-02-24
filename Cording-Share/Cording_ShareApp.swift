a7fec2aac77d16d31ea9f47492c5234f4c573d94
den0206 <mac.pop@mineo.jp>
2021-02-24 23:56:09 +0900

    init FCM

--- Cording-Share/Cording_ShareApp.swift
+++ Cording-Share/Cording_ShareApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Firebase
+import FirebaseMessaging
 
 public var testMode = true
 
@@ -35,10 +36,71 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
+        if #available (iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        Messaging.messaging().delegate = self
+        application.registerForRemoteNotifications()
+        
         return true
     }
     
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return AppDelegate.orientationLock
     }
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        
+        print("Firebase registration token: \(String(describing: fcmToken))")
+
+    }
+
+}
+
+@available(iOS 10, *)
+extension AppDelegate : UNUserNotificationCenterDelegate {
+   func userNotificationCenter(_ center: UNUserNotificationCenter,
+                               willPresent notification: UNNotification,
+                               withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+       let userInfo = notification.request.content.userInfo
+
+       if let messageID = userInfo["gcm.message_id"] {
+           print("Message ID: \(messageID)")
+       }
+
+       print(userInfo)
+
+       completionHandler([])
+   }
+
+   func userNotificationCenter(_ center: UNUserNotificationCenter,
+                               didReceive response: UNNotificationResponse,
+                               withCompletionHandler completionHandler: @escaping () -> Void) {
+       let userInfo = response.notification.request.content.userInfo
+       if let messageID = userInfo["gcm.message_id"] {
+           print("Message ID: \(messageID)")
+       }
+
+       print(userInfo)
+
+       completionHandler()
+   }
 }
